68ead7d5ef0c3f231a53db23df2e4321d1a6a098
CamillaLucero23 <camillalucero23@gmail.com>
2024-02-18 14:35:16 -0700

    Update Disassembler.py

--- Disassembler/Disassembler.py
+++ Disassembler/Disassembler.py
@@ -36,14 +36,14 @@ def string_to_register(string):
 
     return register
 
-
 #Packages
 import os
 
 #This is the file path of the file you wish to decode. Change this! VVV
 hex_file_path = 'C:\\Classes\\2023-2024\\CS 4200 - Comp. Arch\\CS 4200 Repo\\Disassembler\\decode.hex'
-hex_file = '' #This will store a file object if filepath is valid
 
+
+hex_file = '' #This will store a file object if filepath is valid
 #if file path is valid, open it
 print("Checking current directory for specified file: " + hex_file_path)
 if os.path.isfile(hex_file_path):
@@ -163,6 +163,7 @@ while string_index < len(file_contents):
    elif instruction_bytes[0] == '3':
        
        #Get our destination & the value we are putting in that register
+        #We skip a byte because it is not needed for our purposes. It is just a placeholder
         destination = string_to_register(file_contents[string_index+1])
         value = file_contents[(string_index+2):(string_index+2)+6]
         string_index += 8 #don't forget to iterate index to match
@@ -214,9 +215,7 @@ while string_index < len(file_contents):
 
        instruction_string = instruction_string + ' 0x' + destination
 
-
     #Once we are done, print our instruction string
-       
    if len(instruction_string) == 0:
        print("Something went wrong, no instruction found...")
     
